fb026da8115b499a9910255c09423c99d8d6445b
Lucca Gontijo Assis <38539362+Luccagg@users.noreply.github.com>
2025-03-15 21:18:35 -0300

    Update dispersion.py

--- dispersion.py
+++ dispersion.py
@@ -5,4 +5,11 @@ friends = [ 70, 65, 72, 63, 71, 64, 60, 64, 67]
 minutes = [175, 170, 205, 120, 220, 130, 105, 145, 190]
 labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
 plt.scatter(friends, minutes)
+#plt.show()
+for i, j, k in zip(labels, friends, minutes):
+    plt.annotate(i, xy=(j, k), xytext=(5,5),textcoords='offset points')
+
+plt.title("Diary minutes vs friends number")
+plt.xlabel("# friends")
+plt.ylabel("min passed in site")
 plt.show()
