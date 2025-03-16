2128f662f7cb749e9c016d917ccf2ae2863d224d
Lucca Gontijo Assis <38539362+Luccagg@users.noreply.github.com>
2025-03-15 21:09:36 -0300

    Add files via upload

---
+++ dispersion.py
@@ -0,0 +1,8 @@
+from matplotlib import pyplot as plt
+
+# DATA
+friends = [ 70, 65, 72, 63, 71, 64, 60, 64, 67]
+minutes = [175, 170, 205, 120, 220, 130, 105, 145, 190]
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+plt.scatter(friends, minutes)
+plt.show()
